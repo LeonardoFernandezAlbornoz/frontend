a3d7067e4c8d1c0d92d6f1aee374a91975ebb0f2
LeonardoFernandezAlbornoz <a22leonardofa@iessanclemente.net>
2024-06-01 15:15:29 +0200

    api

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ const notivue = createNotivue(/* Options */);
 library.add(fas, far, fab);
 
 const app = createApp(App);
-app.config.globalProperties.backend = 'http://209.38.38.40';
+app.config.globalProperties.backend = 'https://apitienda.casacam.net';
 
 app.component('Navbar', Navbar);
 app.component('FooterVue', FooterVue);
