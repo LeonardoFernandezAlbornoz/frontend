6d65792af601e4038e29ab98c79147d0575f8c22
LeonardoFernandezAlbornoz <a22leonardofa@iessanclemente.net>
2024-06-03 23:00:44 +0200

    03/06/2024

--- src/views/CarritoView.vue
+++ src/views/CarritoView.vue
@@ -8,7 +8,7 @@ const emit = defineEmits(['actualizarProductos']);
 <template>
   <main>
     <Ruta :rutas="[{ to: { name: 'carrito' }, texto: 'Carrito' }]" />
-    <div class="container bg-transparent px-4 px-lg-0">
+    <div class="container-lg bg-transparent px-4 px-lg-0">
       <TituloPagina :titulo="'Mi carrito'"> </TituloPagina>
       <ProductosCarrito
         @actualizarProductos="$emit('actualizarProductos')"
