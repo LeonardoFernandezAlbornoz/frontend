dd74be4b47b4d943a0d2ab2104adede741b04e15
Leonardo Fernandez <leonardo.fernandez@EVELB.LOCAL>
2024-06-07 09:22:34 +0200

    06/06/2024

---
+++ src/components/home/Carousel.vue
@@ -0,0 +1,61 @@
+<script>
+export default {};
+</script>
+<template>
+  <div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="0"
+        class="active"
+      ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="1"
+      ></button>
+      <button
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to="2"
+      ></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="/img/banners/nvidia.jpg" class="d-block w-100" alt="img1" />
+      </div>
+      <div class="carousel-item">
+        <img src="/img/banners/airpods.jpg" class="d-block w-100" alt="img2" />
+      </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="img3" />
+      </div>
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<style>
+.carousel-item img {
+  height: 100%; /* Bootstrap handles width already */
+  object-fit: cover; /* or 'contain' if you want stretch instead of crop */
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,107 +1,107 @@
-import HomeView from '@/views/HomeView.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from "@/views/HomeView.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/productos/categoria/:nomCategoria',
-      name: 'categoria',
+      path: "/productos/categoria/:nomCategoria",
+      name: "categoria",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/CategoriaView.vue'),
+      component: () => import("../views/CategoriaView.vue"),
     },
     {
-      path: '/productos/buscar/:busqueda/:nomCategoria?/',
-      name: 'busqueda',
+      path: "/productos/buscar/:busqueda/:nomCategoria?/",
+      name: "busqueda",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/BusquedaView.vue'),
-    },
-
-    {
-      path: '/producto/:nomCategoria/:nomProducto',
-      name: 'producto',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/ProductoView.vue'),
+      component: () => import("../views/BusquedaView.vue"),
     },
 
     {
-      path: '/perfil',
-      name: 'perfil',
+      path: "/producto/:nomCategoria/:nomProducto",
+      name: "producto",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/PerfilView.vue'),
+      component: () => import("../views/ProductoView.vue"),
     },
 
     {
-      path: '/pedido/:idPedido',
-      name: 'pedido',
+      path: "/perfil",
+      name: "perfil",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/PedidoView.vue'),
-    },
-    {
-      path: '/carrito',
-      name: 'carrito',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/CarritoView.vue'),
+      component: () => import("../views/PerfilView.vue"),
     },
 
     {
-      path: '/pasarela-pago',
-      name: 'pasarela',
+      path: "/pedido/:idPedido",
+      name: "pedido",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/PasarelaView.vue'),
+      component: () => import("../views/PedidoView.vue"),
     },
     {
-      path: '/panel-gestion',
-      name: 'panelGestion',
+      path: "/carrito",
+      name: "carrito",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/PanelGestionView.vue'),
-    },
-
-    {
-      path: '/panel-gestion/gestion-inventario',
-      name: 'gestionInventario',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/InventarioView.vue'),
+      component: () => import("../views/CarritoView.vue"),
     },
 
     {
-      path: '/panel-gestion/gestion-usuarios',
-      name: 'gestionUsuarios',
+      path: "/pasarela-pago",
+      name: "pasarela",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/UsuariosView.vue'),
+      component: () => import("../views/PasarelaView.vue"),
     },
     {
-      path: '/panel-gestion/gestion-pedidos',
-      name: 'gestionPedidos',
+      path: "/panel-gestion",
+      name: "panelGestion",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/GestionsPedidos.vue'),
+      component: () => import("../views/PanelGestionView.vue"),
+    },
+
+    {
+      path: "/panel-gestion/gestion-inventario",
+      name: "gestionInventario",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/InventarioView.vue"),
+    },
+
+    {
+      path: "/panel-gestion/gestion-usuarios",
+      name: "gestionUsuarios",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/UsuariosView.vue"),
+    },
+    {
+      path: "/panel-gestion/gestion-pedidos",
+      name: "gestionPedidos",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/GestionPedidos.vue"),
     },
   ],
 });
